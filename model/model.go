6efa72146111f56258f1440781cef9c4d18a3a2b
Matthew McPherrin <git@mcpherrin.ca>
2025-06-09 21:22:18 -0400

    Try a new eye movement
    this keeps the pupils in the same place, so they don't move independently,
    which seemed a bit weird.
    
    There's no more cross-eye effect, which we might want to bring back.

--- model/model.go
+++ model/model.go
@@ -43,28 +43,28 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m Model) View() string {
 	eyeLX := m.winWidth / 4
-	eyeRX := eyeLX + m.winWidth/2
 	eyeY := m.winHeight / 2
 	eyeRadius := m.winHeight / 2
 	if eyeRadius > m.winWidth/2 {
 		eyeRadius = m.winWidth / 2
 	}
 
-	pupilVecLX, pupilVecLY := vec(eyeLX, eyeY, m.mouseX, m.mouseY)
-	pupilVecLX, pupilVecLY = clamp(eyeRadius/3, pupilVecLX, pupilVecLY)
-	pupilVecRX, pupilVecRY := vec(eyeRX, eyeY, m.mouseX, m.mouseY)
-	pupilVecRX, pupilVecRY = clamp(eyeRadius/3, pupilVecRX, pupilVecRY)
+	var pupilVecX, pupilVecY int
+	if m.winWidth == 0 || m.winHeight == 0 {
+		pupilVecX = 0
+		pupilVecY = 0
+	} else {
+		pupilVecX = int((float32(m.mouseX-(m.winWidth/2)) / float32(m.winWidth/2)) * float32(eyeRadius/2))
+		pupilVecY = int((float32(m.mouseY-(m.winHeight/2)) / float32(m.winHeight/2)) * float32(eyeRadius/2))
+	}
 
 	s := state{
 		eyeLX:     eyeLX,
-		eyeRX:     eyeRX,
 		eyeY:      eyeY,
 		eyeRadius: eyeRadius,
 
-		pupilVecLX: pupilVecLX,
-		pupilVecLY: pupilVecLY,
-		pupilVecRX: pupilVecRX,
-		pupilVecRY: pupilVecRY,
+		pupilVecX: pupilVecX,
+		pupilVecY: pupilVecY,
 	}
 
 	var art strings.Builder
@@ -97,38 +97,13 @@ func inCircle(x, y, centerX, centerY, radius int) bool {
 	return dX*dX+dY*dY < radius*radius
 }
 
-func vec(fromX, fromY, toX, toY int) (int, int) {
-	return toX - fromX, toY - fromY
-}
-
-// clamp magnitude of vector
-func clamp(maxMag, x, y int) (int, int) {
-	// TODO: this should do a vector thing
-	if x > maxMag {
-		x = maxMag
-	}
-	if x < -maxMag {
-		x = -maxMag
-	}
-	if y > maxMag {
-		y = maxMag
-	}
-	if y < -maxMag {
-		y = -maxMag
-	}
-	return x, y
-}
-
 type state struct {
 	eyeLX     int
-	eyeRX     int
 	eyeY      int
 	eyeRadius int
 
-	pupilVecLX int
-	pupilVecLY int
-	pupilVecRX int
-	pupilVecRY int
+	pupilVecX int
+	pupilVecY int
 }
 
 // eye returns the character to render at an x,y point
@@ -140,19 +115,14 @@ func (m Model) eye(x, y int, s state) byte {
 		}
 	}
 
-	// Left pupil:
-	if inCircle(x, y, s.eyeLX+s.pupilVecLX, s.eyeY+s.pupilVecLY, s.eyeRadius/3) {
-		return 'X'
-	}
-
-	// Right pupil:
-	if inCircle(x, y, s.eyeRX+s.pupilVecRX, s.eyeY+s.pupilVecRY, s.eyeRadius/3) {
-		return 'X'
-	}
-
-	// the rest of the eye is symmetric
+	// the eyes are symmetric
 	if x > m.winWidth/2 {
 		x = x - m.winWidth/2
+	}
+
+	// Pupil:
+	if inCircle(x, y, s.eyeLX+s.pupilVecX, s.eyeY+s.pupilVecY, s.eyeRadius/3) {
+		return 'X'
 	}
 
 	// Iris:
